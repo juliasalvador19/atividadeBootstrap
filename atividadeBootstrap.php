892fd7192e1f824196f40b817a9448e58a8e8f87
juliasalvador19 <juliasalvador10@gmail.com>
2022-09-01 19:25:09 -0300

    CalculadoraBootstrapFinalizada

--- atividadeBootstrap.php
+++ atividadeBootstrap.php
@@ -1,26 +1,26 @@
 <?php
-$num1 = $_POST['num1']; //Variavel que irá receber o valor recebido do formulário
-$num2 = $_POST['num2']; //Variavel que irá receber o valor recebido do form
-$calculo = $_POST['opcoes']; // pegara as opções dos botões a selecionar
+$num1 = $_POST['num1']; 
+$num2 = $_POST['num2']; 
+$calculo = $_POST['opcoes']; 
 
-switch($calculo){ // Aqui ele realizará uma série de comparações em relação a operação inserida pelo usuário
+switch($calculo){ 
     case 'adicao':
         $resultado = $num1 + $num2;
-        echo "O resultado é: $resultado";
+        echo "O resultado de $num1 + $num2 é: $resultado";
         break;
     case 'subtracao': 
         $resultado = $num1 - $num2;
-        echo "O resultado é: $resultado";
+        echo "O resultado de $num1 - $num2 é: $resultado";
         break;
     case 'multiplicacao':  
         $resultado = $num1 * $num2;
-        echo "O resultado é: $resultado";
+        echo "O resultado de $num1 * $num2 é: $resultado";
         break;
     case 'divisao':
         $resultado = $num1 / $num2;
-        echo "O resultado é: $resultado";
+        echo "O resultado de $num1 / $num2 é: $resultado";
         break;
     default:
-    echo "Selecione uma opção";
+        echo "Selecione uma opção";
     }
 
